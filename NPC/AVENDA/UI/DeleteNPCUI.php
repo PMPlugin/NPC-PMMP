7231d50136f1fa7ec374f69113b24f54ecdccd98
jun04 <jun040329@gmail.com>
2018-06-23 00:35:10 +0900

    Update DeleteNPCUI.php

--- NPC/AVENDA/UI/DeleteNPCUI.php
+++ NPC/AVENDA/UI/DeleteNPCUI.php
@@ -2,9 +2,11 @@
 
 namespace AVENDA\UI;
 
+use AVENDA\Main;
+
 class DeleteNPCUI {
 	private $owner;
-	public function __construct(\AVENDA\Main $owner) {
+	public function __construct(Main $owner) {
 		$this->owner = $owner;
 	}
 	public function dnpc() {
